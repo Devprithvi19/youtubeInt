4dee18265704a9cdf7cdb42e018d24bb6fd3db45
prithvi <dev.prithvi.19@gmail.com>
2019-03-25 18:04:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/youtubeint/Config.java
@@ -0,0 +1,10 @@
+package com.example.youtubeint;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyBtN_TUTfmt8O535X1T7rmkjmowW0xS8iM";
+
+}
